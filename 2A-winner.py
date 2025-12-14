ad8893084688fe0e0286a8eed2c8869eb8f0ac32
Areeb <areebamir1120@gmail.com>
2025-12-14 13:09:18 -0500

    First codeforce file commit into github

---
+++ 2A-winner.py
@@ -0,0 +1,37 @@
+
+n = int(input())
+
+rounds = []
+for _ in range(n):
+    name, score = input().split()
+    score = int(score)
+    rounds.append((name, score))
+
+final_scores = {}
+
+for name, score in rounds:
+    if name not in final_scores:
+        final_scores[name] = 0
+    final_scores[name] += score
+
+max_score = max(final_scores.values())
+
+candidates = []
+
+for name, total in final_scores.values():
+    if total == max_score:
+        candidates.append(name)
+
+current_scores = {}
+winner = None
+
+for name, score in rounds:
+    if name not in current_scores:
+        current_scores[name] = 0
+    current_scores[name] += score
+
+    if name in candidates and current_scores[name] >= max_score:
+        winner = name
+        break
+
+print(winner)
