e6702aff98ecb9c64e3ab5dba8af3d8b9fb175c6
Areeb <areebamir1120@gmail.com>
2025-12-18 16:24:39 -0500

    Solve the watermelon problem

---
+++ A-watermelon.py
@@ -0,0 +1,15 @@
+
+# 18/12/2025
+
+w = int(input())
+
+if w % 2 == 0:
+    if w == 2:
+        print("NO")
+    else:
+        print("YES")
+else:
+    print("NO")
+
+
+# This was one of the easiest codeforce problem I solved by own without looking at the solutions. Feel motivated!!
