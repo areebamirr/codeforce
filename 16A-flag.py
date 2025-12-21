e09073e659b39927ca4788eb3e45c64d74b4c4be
Areeb <areebamir1120@gmail.com>
2025-12-21 13:50:30 -0500

    Adding the file of 16A-flag problem

---
+++ 16A-flag.py
@@ -0,0 +1,25 @@
+
+n,m = map(int, input().split())
+
+flag = []
+for i in range(n):
+    row = input().strip()
+    flag.append(row)
+
+valid = True
+for i in range(n):
+    if flag[i] != flag[i][0]*m:
+        valid = False
+        break
+
+if valid:
+    for i in range(n-1):
+        if flag[i][0] == flag[i+1][0]:
+            valid = False
+            break
+
+print("YES" if valid else "NO")
+
+
+
+
