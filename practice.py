f67ad07aa431c88835913643c9c35bd6175b08d5
Areeb <areebamir1120@gmail.com>
2026-01-02 19:38:25 -0500

    Save the 5A problem without successfull submission

---
+++ practice.py
@@ -0,0 +1,27 @@
+
+n, m = map(int, input().split())
+rows = []
+min_row = n 
+max_row = -1
+min_col = m
+max_col = -1
+
+for _ in range(n):
+    row = input()
+    rows.append(row)
+
+for i in range(n):
+    for j in range(m):
+        if rows[i][j] == '*':
+            min_row = min(min_row, i)
+            max_row = max(max_row, i)
+            min_col = min(min_col, j)
+            max_col = max(max_col, j)
+
+for i in range(min_row, max_row+1):
+    for j in range(min_col, max_col+1):
+        print(rows[i][j], end='')
+    print()
+
+
+
