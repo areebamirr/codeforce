7e07fda281325ded47abfc8dbb619285e920e0e0
Areeb <areebamir1120@gmail.com>
2026-01-07 06:37:52 -0500

    Submit 9A problem successfully into codeforce by own

---
+++ 9A-DieRoll.py
@@ -0,0 +1,23 @@
+
+from fractions import Fraction
+
+y, w = map(int, input().split())
+
+maxs = y if y > w else w
+
+events = 6
+sample = 0
+for i in range(maxs, events+1):
+    sample += 1
+
+
+if sample/events:
+    if sample/events == 1:
+        print("1/1")
+    else:
+        print(Fraction(sample, events))
+
+elif sample == 0:
+    print("0/1")
+
+
