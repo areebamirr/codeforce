4438ff09fd74427687b9296de29acc8394c773e3
Areeb <areebamir1120@gmail.com>
2026-01-31 21:09:21 -0500

    string problem

--- 71A.WayTooLongWords.py
+++ 71A.WayTooLongWords.py
@@ -1,1 +1,14 @@
-A. Way Too Long Words
+
+# 31/06/2026 String problem. 
+
+n = int(input())
+
+for _ in range(n):
+    word = input()
+    length = len(word)
+    if length > 10:
+        print(f"{word[0]}{length - 2}{word[-1]}")
+    else:
+        print(word)
+    
+
